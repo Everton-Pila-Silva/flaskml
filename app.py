98c7eaf610eb91efeb99f31dbff04bd1afe09ba5
matheusfrancoifmachado <matheus.franco@ifsuldeminas.edu.br>
2023-10-16 14:11:39 -0300

    commit

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 import pickle
 
-application = Flask(__name__)
-CORS(application)
+app = Flask(__name__)
+CORS(app)
 model = pickle.load(open("model.pkl", "rb"))
 names = pickle.load(open("names.pkl", "rb"))
 
-@application.route("/")
+@app.route("/")
 def home():
     return render_template("index.html")
 
-@application.route("/predict", methods=["POST"])
+@app.route("/predict", methods=["POST"])
 def predict():
     features = [float(x) for x in request.form.values()]
     final_features = [np.array(features)]
@@ -20,7 +20,7 @@ def predict():
     output = names[pred[0]]
     return render_template("index.html", prediction_text="Iris " + output)
 
-@application.route("/api", methods=["POST"])
+@app.route("/api", methods=["POST"])
 def results():
     data = request.get_json(force=True)
     pred = model.predict([np.array(list(data.values()))])
